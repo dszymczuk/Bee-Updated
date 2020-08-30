189dcf18aeaadb8a8915a210b7d0978eb954ebf1
Damian <kontakt@dszymczuk.pl>
2020-08-30 11:49:20 +0200

    update color palette

--- src/index.js
+++ src/index.js
@@ -2,30 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-import { amber } from '@material-ui/core/colors';
+import { amber, orange } from '@material-ui/core/colors';
 import { MuiThemeProvider } from "@material-ui/core";
 
 
 const theme = createMuiTheme({
   palette: {
+    // primary: {
+    //   light: '#757ce8',
+    //   main: amber.A700,
+    //   dark: '#002884',
+    //   contrastText: '#fff',
+    // },
+    // secondary: {
+    //   light: '#ff7961',
+    //   main: '#f44336',
+    //   dark: '#ba000d',
+    //   contrastText: '#000',
+    // },
     primary: {
-      light: '#757ce8',
-      main: amber["400"],
-      dark: '#002884',
+      ...orange,
       contrastText: '#fff',
     },
-    secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
-    },
+    secondary: amber,
   },
 });
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <App />
+    <App/>
   </MuiThemeProvider>
   , rootElement);
