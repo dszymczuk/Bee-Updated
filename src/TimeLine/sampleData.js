333ba09bbc444cd3702a72f20b81d3cb20622932
Damian <kontakt@dszymczuk.pl>
2020-08-30 12:11:43 +0200

    add sample data

--- src/TimeLine/sampleData.js
+++ src/TimeLine/sampleData.js
@@ -80,39 +80,42 @@ export default [
     </Fragment>,
   },
   {
+    avatar: <img src="/sandwich.svg" style={{ height: 35 }}/>,
+    title: "Sandwich shop arrived 🥪",
+    date: "",
+    image: "/sandwiches.jpg",
+    text: <Fragment>
+      <Typography paragraph>Grilled Cheese w/ side of chips - $8.49
+        <Typography variant="body2">Pepper Jack, Mozzarella, Provolone, Cheddar, Bacon Bits</Typography>
+      </Typography>
+
+      <Typography paragraph>Mama Mia w/ side of chips - $8.49
+        <Typography variant="body2">Pepperoni, Salami, Mozzarella, Served w/ side of marinara</Typography>
+      </Typography>
+
+      <Typography paragraph>Spicy Chicken w/ side of chips - $8.49
+        <Typography variant="body2">Chicken Breast, Bacon, Pepper Jack Cheese, Avocado-Poblano Ranch Dressing</Typography>
+      </Typography>
+
+      <Typography paragraph>Turkey Melt w/ side of chips - $8.49
+        <Typography variant="body2">Turkey, Bacon, Pepper Jack Cheese, w/ choice of Hot Chipotle Mayo Or Sweet N Tangy Chipotle Ranch</Typography>
+      </Typography>
+    </Fragment>,
+    extraText: "",
+  },
+  {
     avatar:
       <Fragment>
         <img src="https://picsum.photos/id/1005/100/100"/>
       </Fragment>,
-    title: "Title",
+    title: "Bike trip this weekend 🚲",
     date: "2.09.2020",
-    image: `https://picsum.photos/seed/${randomIntFromInterval()}/800/500`,
-    text: "Text",
+    image: "/bike_forest.jpg",
+    text:
+      <Typography paragraph>
+        You can register here.
+      </Typography>,
     color,
-    extraText: <div>
-      <Typography paragraph>Method:</Typography>
-      <Typography paragraph>
-        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-        minutes.
-      </Typography>
-      <Typography paragraph>
-        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-        heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-        browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-        and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-        pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-      </Typography>
-      <Typography paragraph>
-        Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-        without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-        medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-        again without stirring, until mussels have opened and rice is just tender, 5 to 7
-        minutes more. (Discard any mussels that don’t open.)
-      </Typography>
-      <Typography>
-        Set aside off of the heat to let rest for 10 minutes, and then serve.
-      </Typography>
-    </div>,
+    extraText: "",
   }
 ]
