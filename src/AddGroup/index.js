8f63fd6e8ee2366749b3fdb149718695b5e200f1
Damian <kontakt@dszymczuk.pl>
2020-08-30 22:10:47 +0200

    remove console.log

--- src/AddGroup/index.js
+++ src/AddGroup/index.js
@@ -9,7 +9,7 @@ import IconButton from "@material-ui/core/IconButton";
 
 const avatarSize = 100;
 
-const useStyles = makeStyles((theme) => (console.log(theme),{
+const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(2),
